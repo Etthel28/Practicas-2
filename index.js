cf0dc9b4a1016f79444b327defcabe172604a495
practicasEthel <ethel15.sp@gmail.com>
2023-11-24 16:11:58 -0300

    clase terminada

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ let atributo = prompt("Ingrese un atributo");
 console.log(persona[atributo]);*/
 
 //funciones constructoras
-function persona(nombrecito, cumpleañito, dondevive) {
+/*function persona(nombrecito, cumpleañito, dondevive) {
     this.nombre = nombrecito
     this.edad = cumpleañito
     this.calle = dondevive
@@ -65,4 +65,101 @@ function persona(nombrecito, cumpleañito, dondevive) {
 //console.log(this.nombre)
 const instanciaPersona = new persona("Ethel", 27, "San Pedro")
 instanciaPersona.saludar();
+*/
 
+/*function persona(nombrecito, cumpleañito, dondevive) {
+    this.nombre = nombrecito
+    this.edad = cumpleañito
+    this.calle = dondevive
+    this.saludar = () => {
+        alert("Hola, mi nombre es: " + this.nombre)
+    }
+}/*
+
+
+//console.log(this.nombre)
+const instanciaPersona = new persona("Ethel", 27, "San Pedro")
+//instanciaPersona.saludar();
+
+
+
+
+
+
+//operador IN-------------------------
+//console.log("edad" in instanciaPersona);
+
+
+//operador for in------------
+let propiedad
+for (propiedad in instanciaPersona) {
+    console.log("Atributo: " + propiedad)
+    console.log(instanciaPersona[propiedad])
+}*/
+
+
+
+
+
+
+//-------------------------------------------------------
+//clase------------ este tipo de sintaxis es la que vamos a usar
+
+
+/*class Persona {
+    constructor(nombrecito, cumpleañito, dondevive) {
+        this.nombre = nombrecito
+        this.edad = cumpleañito
+        this.calle = dondevive
+    }
+    saludar() {
+        console.log("Hola, soy " + this.nombre + ", y tengo " + this.edad + " años de edad")
+    }
+}
+const instanciaPersona2= new Persona("Ethel", 27, "San Pedro")
+instanciaPersona2.saludar()
+*/
+
+//Ejemplo de cuenta bancaria
+
+class cuentaBancaria {
+    constructor(titular, cuenta, saldo, limite) {
+        this.nombreTitular = titular
+        this.nroCuenta = cuenta
+        if (saldo >= 0) {
+            this.saldo
+        } else {
+            this.saldo = 0
+        }
+        if (limite >= 150000) {
+            this.limite
+        } else {
+            this.limite = 150000
+        }
+    }
+
+
+    extraer(monto) {
+        if (monto > 0) {
+            this.saldo = this.saldo - monto
+            if (monto <= this.saldo) {
+                this.saldo -= monto
+                rest = true
+            } else {
+                alert("No se puede extrae mas de los que hay")
+                resul = false
+
+
+            }
+        }
+        else {
+            alert("No se puedes extraer montos negativos")
+            resul = false
+        }
+
+        return res
+    }
+
+    
+   
+} 
